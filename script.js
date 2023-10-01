fa7f83ec7a0ff50fce0fbf66c01446c2c2f9dca2
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-10-01 21:44:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const DivElement=document.querySelector(".container")
+const pagination=document.querySelector("#Pagination")
+
+
+pagination.innerHTML+=`<button class="btn1">Previous<button>
+<ul>
+<li class="link active" ><a class="page-link"href="#"></a>1</li>
+<li class="link active"><a class="page-link" href="#"></a>2</li>
+<li class="link active"><a class="page-link" href="#"></a>3</li>
+<li class="link active""><a class="page-link" href="#"></a>4</li>
+<li class="link active""><a class="page-link" href="#"></a>5</li>
+<li class="link active""><a class="page-link" href="#"></a>6</li>
+<li class="link active""><a class="page-link" href="#"></a>7</li>
+<li class="link active""><a class="page-link" href="#"></a>8</li>
+<li class="link active""><a class="page-link" href="#"></a>9</li>
+<li class="link active""><a class="page-link" href="#"></a>10</li>
+</ul>
+<button class="btn2">Next<button>`
+
