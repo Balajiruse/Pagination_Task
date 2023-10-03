395ece42b00e09bf210cc50294cd91389a939f9f
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-10-03 19:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const DivElement=document.querySelector(".container")
 const pagination=document.querySelector("#Pagination")
 
 
-pagination.innerHTML+=`<button class="btn1">Previous<button>
+
+pagination.innerHTML+=`<button class="btn1"><a class="page-link" href="#">Previous</a><button>
+<nav aria-label="Page navigation example">
 <ul>
-<li class="link active" ><a class="page-link"href="#"></a>1</li>
-<li class="link active"><a class="page-link" href="#"></a>2</li>
-<li class="link active"><a class="page-link" href="#"></a>3</li>
-<li class="link active""><a class="page-link" href="#"></a>4</li>
-<li class="link active""><a class="page-link" href="#"></a>5</li>
-<li class="link active""><a class="page-link" href="#"></a>6</li>
-<li class="link active""><a class="page-link" href="#"></a>7</li>
-<li class="link active""><a class="page-link" href="#"></a>8</li>
-<li class="link active""><a class="page-link" href="#"></a>9</li>
-<li class="link active""><a class="page-link" href="#"></a>10</li>
+<li class="page-item"><a class="page-link"href=https://www.guvi.in/courses/?current_tab=myCours" target="_blank">1</a></li>
+<li class="page-item"><a class="page-link" href="https://www.w3schools.com">2</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">3</a></li>
+<li class="page-item"><a class="page-link" href="https://www.google.com/" target="_blank">4</a></li>
+<li class="page-item"><a class="page-link" href="https://www.zenclass.in/" target="_blank">5</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">6</a></li>
+<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >7</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">8</a></li>
+<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >9</a></li>
+<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >10</a></li>
 </ul>
-<button class="btn2">Next<button>`
+</nav>
+
+
+<button class="btn2"><a class="page-link" href="#">Next</a><button>`
+
 
