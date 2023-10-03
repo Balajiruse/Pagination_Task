7d714e04a7da0581cea04eb50350dbf254e3f230
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-10-03 19:32:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ const pagination=document.querySelector("#Pagination")
 
 
 
-pagination.innerHTML+=`<button class="btn1"><a class="page-link" href="#">Previous</a><button>
+pagination.innerHTML+=`<button class="btn1">Previous<button>
 <nav aria-label="Page navigation example">
 <ul>
 <li class="page-item"><a class="page-link"href=https://www.guvi.in/courses/?current_tab=myCours" target="_blank">1</a></li>
 <li class="page-item"><a class="page-link" href="https://www.w3schools.com">2</a></li>
 <li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">3</a></li>
 <li class="page-item"><a class="page-link" href="https://www.google.com/" target="_blank">4</a></li>
-<li class="page-item"><a class="page-link" href="https://www.zenclass.in/" target="_blank">5</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours"target="_blank">5</a></li>
 <li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">6</a></li>
-<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >7</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">7</a></li>
 <li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">8</a></li>
-<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >9</a></li>
-<li class="page-item"><a class="page-link" href=https://www.zenclass.in/" >10</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">9</a></li>
+<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">10</a></li>
 </ul>
 </nav>
-
-
-<button class="btn2"><a class="page-link" href="#">Next</a><button>`
+<button class="btn2">Next<button>`
 
 
