dfa1d7d6e81fcee11137b3dcf4ab7430c2b5ade1
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-10-04 22:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const pagination=document.querySelector(".Pagination")
 
-
-
 pagination.innerHTML+=`
 
 
@@ -11,16 +9,16 @@ pagination.innerHTML+=`
 
 <nav aria-label="Page navigation example">
 <ul>
-<li class="page-item"><a class="page-link"href=https://www.guvi.in/courses/?current_tab=myCours" target="_blank">1</a></li>
-<li class="page-item"><a class="page-link" href="https://www.w3schools.com">2</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">3</a></li>
-<li class="page-item"><a class="page-link" href="https://www.google.com/" target="_blank">4</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours"target="_blank">5</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">6</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">7</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">8</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">9</a></li>
-<li class="page-item"><a class="page-link" href="https://www.guvi.in/courses/?current_tab=myCours" target="_blank">10</a></li>
+<li class="page-item"><a class="page-link"href="#">1</a></li>
+<li class="page-item"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>
+<li class="page-item"><a class="page-link" href="#">4</a></li>
+<li class="page-item"><a class="page-link" href="#">5</a></li>
+<li class="page-item"><a class="page-link" href="#">6</a></li>
+<li class="page-item"><a class="page-link" href="#">7</a></li>
+<li class="page-item"><a class="page-link" href="#">8</a></li>
+<li class="page-item"><a class="page-link" href="#">9</a></li>
+<li class="page-item"><a class="page-link" href="#">10</a></li>
 </ul>
 </nav>
 
